b2db7bab9c7a1dc3e8f6c6b8ae83a324442ab76c
Nabeel77 <nabeel.beeni30@gmail.com>
2022-01-05 12:49:01 +0200

    fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,4 +27,21 @@ export default class MyDocument extends Document {
       sheet.seal()
     }
   }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@700&display=swap" rel="stylesheet"/> 
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+
 }
