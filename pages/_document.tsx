60b4b14cab839c2533219ce9c0c90e04ffcba61a
Nabeel77 <nabeel.beeni30@gmail.com>
2022-01-05 12:52:58 +0200

    fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,9 +32,9 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@700&display=swap" rel="stylesheet"/> 
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+          <link href="https://fonts.googleapis.com/css2?family=Orbitron&family=Raleway:wght@200;400;600&display=swap" rel="stylesheet"/>
         </Head>
         <body>
           <Main />
